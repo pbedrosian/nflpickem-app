43174e4026d7f57039a25001c7fc071c05e5ee78
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 15:35:28 -0400

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :picks
+
   root 'users#home'
   
   devise_for :users, path: :accounts
 
-  resources :teams
+  resources :users, only: [:show, :index] do
+    resources :picks
+  end
+
+  # esources :teams
+
+  # resources :games
+
+  get '/profile', to: 'users#show', as:'profile'
+
+  get '/league', to: 'users#index'
+
+  get '/schedule', to: 'games#schedule'
 
 end
