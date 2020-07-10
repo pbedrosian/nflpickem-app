09ebe02db6009ee10122503feaf85fc20d656e3e
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 17:22:07 -0400

    Added Picks controller actions

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -1,2 +1,36 @@
 class PicksController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+        #@games = Game.all.select {|g| g.week == 12} #add current_week method to get current week
+        @pick = current_user.picks.new
+    end
+
+    def create
+        @pick = current_user.picks.new(picks_params)
+        if @pick.save
+            redirect_to user_picks_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @picks = User.find_by_id(params[:user_id]).picks
+    end
+
+    def edit
+        user = User.find_by_id(params[:user_id])
+        @pick = user.picks.find_by_id(params[:id])
+    end
+
+    def update
+        
+    end
+
+    private
+
+    def picks_params
+        params.require(:pick).permit(:team_id, :buy_in)
+    end
 end
