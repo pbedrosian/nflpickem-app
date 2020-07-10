e07f2b3caf20cd8d7a53ae232785fd36db2b8960
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 17:05:42 -0400

    Added weekly_games / matchup helpers for Games

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,11 @@
 module GamesHelper
+
+    def weekly_games
+        Week.current_week.games
+    end
+
+    def matchup(game)
+        "#{game.home_team.name} vs. #{game.away_team.name}"
+    end
+    
 end
