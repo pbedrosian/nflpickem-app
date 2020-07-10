d15b5c380bb8e37309e26c17cd51aebe4de488be
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 15:26:15 -0400

    Added Pick association to user/team

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -1,2 +1,17 @@
 class Pick < ApplicationRecord
+    belongs_to :user
+    belongs_to :team
+  
+    validates :team_id, :presence => true
+  
+    validate :user_quota, :on => :create
+  
+    #if current_user has a pick within week zone, deny new pick. 
+    private 
+  
+    def user_quota
+      if user.picks.this_week.count >= 1
+       errors.add(:base, "Your Pick has already been made, please edit your current pick.")
+     end
+    end
 end
