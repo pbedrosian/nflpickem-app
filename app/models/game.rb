9ce1486bbf3a3d47a4267850b0134b3696cc80cc
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 15:25:21 -0400

    Added Game associations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ApplicationRecord
+    belongs_to :home_team, class_name: 'Team'
+    belongs_to :away_team, class_name: 'Team'
+    belongs_to :week
+   
+    def time
+        self.date.to_time
+    end
 end
