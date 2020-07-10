e1f335150e2c088894a65fe47d598ad464f20d94
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 15:32:06 -0400

    Added Week association games + helpers

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,2 +1,20 @@
-class Week < ApplicationRecord
+class Week < ApplicationRecord  
+    has_many :games
+
+    def self.current_week
+        week = Week.all.select do |w| 
+            if Time.now.between?(w.start_of_week.to_time, w.end_of_week.to_time)
+                w.id
+            end
+        end
+        week[0]
+    end
+
+    def self.search(query)
+        if query.present? 
+            self.find_by_id(query).games
+        else 
+            self.current_week.games
+        end
+    end
 end
