4b5a01629e1667777daea89469955d1ddb6ae002
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 15:27:36 -0400

    Added Team association to picks/users

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,16 @@
-class Team < ApplicationRecord
+class Team < ApplicationRecord 
+    belongs_to :picks
+
+    has_many :users, through: :picks
+
+    has_many :home_games, class_name: "Game", foreign_key: "home_team_id"
+    has_many :away_games, class_name: "Game", foreign_key: "away_team_id"
+
+    def games
+        games = []
+        games << home_games
+        games << away_games
+        games.flatten
+    end
+
 end
