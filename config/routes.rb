caa1d585ded44d915d8acf6405d30cd62e1d81bb
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-10 15:01:45 -0400

    Added Teams resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :teams
   root 'users#home'
   devise_for :users, path: :accounts
 end
---
+++ db/migrate/20200710185847_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :abbreviation
+      t.integer :bye
+
+      t.timestamps
+    end
+  end
+end
