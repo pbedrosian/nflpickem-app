2d75765651ca518154a32304f6f2867cc45c9fda
pbedrosian <peter_bedrosian@Yahoo.com>
2020-07-27 20:07:57 -0400

    Added winner association to team / games

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,8 @@ class Game < ApplicationRecord
     belongs_to :home_team, class_name: 'Team'
     belongs_to :away_team, class_name: 'Team'
     belongs_to :week
+    belongs_to :winner, class_name: 'Team', foreign_key: "winner_id"
+
    
     def time
         self.date.to_time
--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,7 @@ class Team < ApplicationRecord
 
     has_many :home_games, class_name: "Game", foreign_key: "home_team_id"
     has_many :away_games, class_name: "Game", foreign_key: "away_team_id"
+    has_many :winners, class_name: "Game", foreign_key: "winner_id"
 
     def games
         games = []
